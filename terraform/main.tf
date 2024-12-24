2f8ecc2b3f52c6343d44d5bc8711f7f43b2bd59c
Vadiraj-Raikar <vadirajraikar2000@gmail.com>
2024-12-24 09:39:36 +0000

    created the new files

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,22 @@ provider "aws" {
 resource "aws_instance" "instance-1" {
     ami = "ami-0614680123427b75e"
     instance_type ="t2.micro"
-    security_groups = {"default"}
+    security_groups = ["default"]
     key_name = "raju"
     user_data = file("grafana.sh")
     tags = {
         Name = "Grafana-server"
     }
 }
+    
+
+    
+
 
 resource "aws_instance" "instance-2" {
     ami = "ami-0614680123427b75e"
     instance_type ="t2.micro"
-    security_groups = {"default"}
+    security_groups = ["default"]
     key_name = "raju"
     tags = {
         Name = "Node-expo"
